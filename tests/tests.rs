0d7213adbad47cbc11e2c02672dc5d66bf184904
rkr35 <rkr35@drexel.edu>
2019-12-21 15:54:22 -0500

    Add initial tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+
+use core::ptr;
 use static_assertions as sa;
 use std::os::raw::*;
 use vtables::VTable;
@@ -5,7 +8,16 @@ use vtables_derive::{has_vtable, virtual_index, VTable};
 
 #[has_vtable]
 #[derive(VTable, Debug)]
-pub struct EngineClient {
+struct EngineClient {
+}
+
+// This structure will fail to compile if #[has_vtable] added another `vtable` field.
+#[has_vtable]
+struct AlreadyHasVTableField<'a> {
+    vtable: u8, 
+    foo: bool,
+    bar: f32,
+    baz: Option<&'a u8>,
 }
 
 #[allow(non_snake_case)]
@@ -24,6 +36,46 @@ impl EngineClient {
 }
 
 #[test]
-fn pass() {
-    assert!(true);
+fn has_vtable_adds_vtable_field() {
+    // This function will fail to compile if #[has_vtable] does not add a `vtable`.
+    
+    sa::assert_fields!(EngineClient: vtable);
+}
+
+#[test]
+fn derive_vtable_adds_get_virtual_method() {
+    // This function will fail to compile if #[derive(VTable)] did not add a `get_virtual(usize)` method.
+    
+    let engine_client = EngineClient {
+        vtable: ptr::null_mut(),
+    };
+
+    let _f = |i| {
+        type ExampleVirtualMethod = fn(&EngineClient, bool) -> f64;
+        unsafe { engine_client.get_virtual::<ExampleVirtualMethod>(i) };
+    };
+}
+
+#[test]
+fn virtual_index_retains_declared_methods() {
+    // This function will fail to compile if #[virtual_index(...)] fails to emit the method it decorates.
+    
+    let engine_client = EngineClient {
+        vtable: ptr::null_mut(),
+    };
+
+    macro_rules! verify {
+        ($method:ident) => {{
+            verify!($method,)
+        }};
+
+        ($method:ident, $($arg:ident),*) => {{
+            let _f = |$($arg),*| engine_client.$method($($arg),*);
+        }};
+    }
+
+    verify!(GetScreenSize, w, h);
+    verify!(IsInGame);
+    verify!(ExecuteClientCmd, command);
+    verify!(ClientCmd_Unrestricted, command);
 }
